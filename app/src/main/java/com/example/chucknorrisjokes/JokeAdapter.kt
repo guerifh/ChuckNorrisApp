47d464ffae86232baeaf8a25c110b799be701903
guerifh <hugo.guerif@hotmail.fr>
2020-03-05 10:57:12 +0100

    1. Adapter créé 2. Impossible de run l'app

--- app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
+++ app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
@@ -1,3 +1,28 @@
 package com.example.chucknorrisjokes
 
-class JokeAdapter
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class JokeAdapter : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
+
+    var jokeList = jokes.list
+
+    class JokeViewHolder(val text: TextView) : RecyclerView.ViewHolder(text)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
+        val tv: TextView = TextView(parent.context)
+        return JokeViewHolder(tv)
+    }
+
+    override fun getItemCount() = jokeList.size
+
+    override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
+        holder.text.text = jokeList[position]
+    }
+
+    fun setter(list: List<String>) {
+        jokeList = list
+    }
+
+}
