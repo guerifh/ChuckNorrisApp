61ca9950d843595672657e97285badfa4539faa4
GUERIF Hugo <hugo.guerif@hotmail.fr>
2020-03-05 09:14:18 +0100

    1. JokeList créé 2. RecyclerView ajouté 3. Adapter débuté

---
+++ app/src/main/java/com/example/chucknorrisjokes/JokeList.kt
@@ -0,0 +1,12 @@
+package com.example.chucknorrisjokes
+
+val list =
+    listOf<String>(
+        "Chuck Norris was recently asked what he thought of Nancy Pelosi? Chuck said he thinks Nancy Pelosi is uglier than a bucket of buttholes. There were no further questions.",
+        "Once Chuck Norris peed against a fence. A police officer saw him and said - That's against the law! - No, said Chuck. It is against the fence!",
+        "On Day Zero, Chuck Norris created God.",
+        "Chuckeroo = Chuck Norris spam.",
+        "If it looks like chicken, tastes like chicken, and feels like chicken but Chuck Norris says its beef, then it's fucking beef."
+    )
+//Log.d("Liste", list.toString())
+
