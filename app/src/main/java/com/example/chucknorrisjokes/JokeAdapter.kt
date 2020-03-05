e6e6e664a77aad20bfae7e6ec226c70b20990ba4
GUERIF Hugo <hugo.guerif@hotmail.fr>
2020-03-05 11:31:33 +0100

    1. Fin du TP1 2. Modifications du TextView perso

--- app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
+++ app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.chucknorrisjokes
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -11,7 +12,12 @@ class JokeAdapter : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
     class JokeViewHolder(val text: TextView) : RecyclerView.ViewHolder(text)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
-        val tv: TextView = TextView(parent.context)
+        //val tv: TextView = TextView(parent.context)
+        val tv: TextView = LayoutInflater.from(parent.context).inflate(
+            R.layout.text_view,
+            parent,
+            false
+        ) as TextView
         return JokeViewHolder(tv)
     }
 
