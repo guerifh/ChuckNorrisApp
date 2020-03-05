6cfbc90515450cae69bf6ce578423b1e4b80c320
GUERIF Hugo <hugo.guerif@hotmail.fr>
2020-03-05 11:31:33 +0100

    1. Fin du TP1 2. Modifications du TextView perso

--- app/src/main/java/com/example/chucknorrisjokes/JokeList.kt
+++ app/src/main/java/com/example/chucknorrisjokes/JokeList.kt
@@ -7,7 +7,14 @@ object jokes {
             "Once Chuck Norris peed against a fence. A police officer saw him and said - That's against the law! - No, said Chuck. It is against the fence!",
             "On Day Zero, Chuck Norris created God.",
             "Chuckeroo = Chuck Norris spam.",
-            "If it looks like chicken, tastes like chicken, and feels like chicken but Chuck Norris says its beef, then it's fucking beef."
+            "If it looks like chicken, tastes like chicken, and feels like chicken but Chuck Norris says its beef, then it's fucking beef.",
+            "Chuck Norris doesn't drink often. But when he does, he prefers to kick 'the most interesting man in the word' in the ass & take his Dos Equis.",
+            "Why is Phillip's head shaped like a screwdriver? Chuck Norris.",
+            "Chuck Norris eats Spicy Chicken Crunchwraps, made with live chickens.",
+            "When Chuck Norris brews a strong pot of coffee he uses a flame thrower and 1/2 of Columbia's coffee trees.",
+            "Chuck Norris is the way the cookie crumbles.",
+            "Chuck Norris doesn't laugh in the face of danger. He punches it repeatedly until it's the face of fear.",
+            "Chuck Norris' father is Chuck Norris, his mother is America, his brother is freedom and his other brother is Sam... Sam Norris. Chuck Norris loves his family dearly, except for Sam, that is why Sam no longer exists."
         )
 //Log.d("Liste", list.toString())
 
