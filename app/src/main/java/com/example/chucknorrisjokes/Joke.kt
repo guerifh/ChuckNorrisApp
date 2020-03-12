79832582f60859a9ffeed14a174faa40cce4381f
GUERIF Hugo <hugo.guerif@hotmail.fr>
2020-03-12 09:43:37 +0100

    1.Classe de test fonctionnelle 2.Classe Joke modifiée et fonctionnelle
    Prochaine étape :
    Modification des listes dans JokeList.kt

--- app/src/main/java/com/example/chucknorrisjokes/Joke.kt
+++ app/src/main/java/com/example/chucknorrisjokes/Joke.kt
@@ -1,18 +1,34 @@
 package com.example.chucknorrisjokes
 
+import android.util.Log
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 class Joke(
     val categories: List<String>,
-    @SerialName("created_At")
+    @SerialName("created_at")
     val createdAt: String,
-    @SerialName("icon_URL")
+    @SerialName("icon_url")
     val iconUrl: String,
     val id: String,
-    @SerialName("updated_At")
+    @SerialName("updated_at")
     val updatedAt: String,
     val url: String,
     val value: String
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        var bool = true
+        val param: Joke
+        try {
+            param = other as Joke
+        } catch (Error: Exception) {
+            Log.d("Erreur de cast", Error.toString())
+            return false
+        }
+        if (categories != param.categories || createdAt != param.createdAt || iconUrl != param.iconUrl || id != param.id || updatedAt != param.updatedAt || url != param.url || value != param.value) {
+            bool = false
+        }
+        return bool
+    }
+}
